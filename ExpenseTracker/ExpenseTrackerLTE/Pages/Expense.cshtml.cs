62b218d7b824d022000af76d139bf58c0eb29ead
On Le <ale2539669@gmail.com>
2023-10-30 11:32:54 -0700

    Added AdminLTE
    Migrated the AdminLTE template to the project.

---
+++ ExpenseTracker/ExpenseTrackerLTE/Pages/Expense.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExpenseTrackerLTE.Pages
+{
+    public class ExpenseModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
